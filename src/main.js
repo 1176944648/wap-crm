80c2a0b8cfe94c51b71e5560c98a7c1d635958fb
1176944648 <56572999+1176944648@users.noreply.github.com>
2020-01-15 14:55:30 +0800

    项目初始化
    项目初始化

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import Router from "vue-router"
+import Vant from "vant"
+import axios from "axios"
+import 'vant/lib/index.css'
+import createRouter from "./router"
+import "./assets/rem"
+import "./assets/css/main.styl"
+Vue.use(Vant)
 Vue.config.productionTip = false
+Vue.prototype.$axios = axios
+const router = createRouter(Router)
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,8 @@
+import routes from "./routes"
+
+export default function createRouter(Router){
+    return new Router({
+        mode : "hash",
+        routes,
+    })
+}
