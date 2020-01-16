edd789599334a16ea93e39fdd12f18a99540a2b8
zhuhailiang <524577590@qq.com>
2020-01-16 09:51:24 +0800

    客户列表页面

---
+++ src/pages/customerList/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>客户列表</h1>
+        <router-link to="/search">搜索</router-link>
+    </div>
+</template>
+
+<script>
+import Search from "./search/search.vue"
+export default {
+     components: {
+        Search
+  }
+}
+
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,3 +1,15 @@
-const router = []
+import Search from "../pages/customerList/search/search.vue"
+import CustomerList from "../pages/customerList"
+
+const router = [{
+    path:"/customerList",
+    name:"customerList",
+    component:CustomerList,
+
+},{
+    path:"/search",
+    name:"search",
+    component:Search,
+}]
 
 export default router
