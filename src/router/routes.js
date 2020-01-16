51bce9e3a522b8119657f049c9c2da910e19f074
soulja <845064382@qq.com>
2020-01-16 09:48:59 +0800

    客户详情
    客户详情三个大路由基本雏形完成

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,3 +1,16 @@
-const router = []
+import Information from "../pages/customerDetails/customerInformation/CustomerInformation.vue"
+import Record from "../pages/customerDetails/documentaryRecord/DocumentaryRecord.vue"
+import Sign from "../pages/customerDetails/signMsg/SignMsg.vue"
+
+const router = [{
+    path: "/Information",
+    component : Information
+},{
+    path: "/Record",
+    component : Record
+},{
+    path: "/Sign",
+    component : Sign
+}]
 
 export default router
