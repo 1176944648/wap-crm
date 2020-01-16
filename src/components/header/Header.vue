b90b90c6cd80a66ca7b2818966e3ccc8a4585d1b
1176944648 <56572999+1176944648@users.noreply.github.com>
2020-01-16 17:00:11 +0800

    头部样式完成

---
+++ src/components/header/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <header>
+    <div class="logo">
+      <img src="../../assets/img/logo.png" alt="">
+    </div>
+    <div class="title">{{title}}</div>
+    <div class="slotBox">
+      <slot></slot>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  props: ["title"]
+};
+</script>
+
+<style lang="stylus" scoped>
+header 
+  height 0.42rem
+  width 100%
+  background #108ee9
+  padding-top .1rem
+  font-size .16rem
+  color #fff
+  .logo 
+    float left
+    margin-left .1rem
+    img 
+      height .24rem
+      width .86rem
+  .title
+    float left
+    margin-left .5rem
+  .slotBox
+    float right
+    margin-right .1rem
+</style>
--- src/pages/bidding/orderList/list.vue
+++ src/pages/bidding/orderList/list.vue
@@ -1,13 +1,17 @@
 <template>
-  <div>空状态/列表</div>
+  <div>
+    <Header title="订单竞价系统"/>
+    <div>空状态/列表</div>
+  </div>
 </template>
 
 <script>
+import Header from "../../../components/header/Header.vue";
 export default {
-
-}
+  components: {
+    Header
+  }
+};
 </script>
 
-<style lang="stylus" scoped>
-
-</style>
+<style lang="stylus" scoped></style>
