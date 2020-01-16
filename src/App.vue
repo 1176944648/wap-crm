b77e89e7689d680806f2460eece2fbd53ca00d2a
1176944648 <56572999+1176944648@users.noreply.github.com>
2020-01-16 10:58:22 +0800

    11

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <template>
   <div id="app">
+<<<<<<< HEAD
 
+=======
+    hello
+>>>>>>> parent of b9ee7ee... Merge branch 'user' of https://github.com/1176944648/wap-crm into user
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-
 export default {
+<<<<<<< HEAD
+=======
+  name: 'app',
+  components: {
+    
+  }
+>>>>>>> parent of b9ee7ee... Merge branch 'user' of https://github.com/1176944648/wap-crm into user
 }
 </script>
 
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,4 +1,7 @@
 import Details from "../pages/customerDetails/CustomerDetails.vue"
+
+import Search from "../pages/customerList/search/search.vue"
+import CustomerList from "../pages/customerList"
 import Information from "../pages/customerDetails/customerInformation/CustomerInformation.vue"
 import Record from "../pages/customerDetails/documentaryRecord/DocumentaryRecord.vue"
 import Sign from "../pages/customerDetails/signMsg/SignMsg.vue"
@@ -20,5 +23,28 @@ const router = [{
     redirect : "/Details/Information"
 },
 ]
+    path: "/Information",
+    component : Information
+},{
+    path: "/Record",
+    component : Record
+},{
+    path: "/Sign",
+    component : Sign
+},{
+    path:"/customerList",
+    name:"customerList",
+    component:CustomerList,
+    children:[{
+        path:"all",
+        name:"customerListAll",
+        component:CustomerListAll
+    }]
+
+},{
+    path:"/search",
+    name:"search",
+    component:Search,
+}]
 
 export default router
