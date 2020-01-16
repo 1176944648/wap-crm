3ed73ad92e682af5c35104487d28065834198621
soulja <845064382@qq.com>
2020-01-16 10:38:01 +0800

    客户详情
    优化了一下进主页

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,16 +1,23 @@
+import Details from "../pages/customerDetails/CustomerDetails.vue"
 import Information from "../pages/customerDetails/customerInformation/CustomerInformation.vue"
 import Record from "../pages/customerDetails/documentaryRecord/DocumentaryRecord.vue"
 import Sign from "../pages/customerDetails/signMsg/SignMsg.vue"
 
 const router = [{
-    path: "/Information",
-    component : Information
-},{
-    path: "/Record",
-    component : Record
-},{
-    path: "/Sign",
-    component : Sign
+    path: "/Details",
+    component : Details,
+    children : [{
+        path: "Information",
+        component : Information
+    },{
+        path: "Record",
+        component : Record
+    },{
+        path: "Sign",
+        component : Sign
+    }],
+    path : "/Details",
+    redirect : "/Details/Information"
 }]
 
 export default router
