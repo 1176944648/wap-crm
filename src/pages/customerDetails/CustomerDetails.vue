c823029f10b74f5d24cd5213a99d58cc42b690a5
soulja <845064382@qq.com>
2020-01-16 19:58:48 +0800

    11
    11

--- src/pages/customerDetails/CustomerDetails.vue
+++ src/pages/customerDetails/CustomerDetails.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <Header title="详情"/>
     <div class="detail">
       <router-link :to="'/Details/Information/'+user">基本信息</router-link>
       <router-link :to="'/Details/Record/'+user">跟单记录</router-link>
@@ -10,8 +11,12 @@
 </template>
 
 <script>
+import Header from "../../components/header/Header.vue"
 import bus from "../../assets/bus"
 export default {
+  components:{
+    Header
+  },
   data(){
     return{
       user:''
@@ -27,17 +32,17 @@ export default {
 <style lang="stylus" scoped>
   .detail
       width 100%
-      height .5rem
+      height 1rem
       display flex
       background #fff
       a
           text-align center
-          font-size .16rem
-          line-height .5rem
+          font-size .32rem
+          line-height 1rem
           flex 1
           color black
-          height .5rem
+          height 1rem
       .router-link-exact-active
           color blue
-          border-bottom .02rem solid blue
+          border-bottom .04rem solid blue
 </style>
