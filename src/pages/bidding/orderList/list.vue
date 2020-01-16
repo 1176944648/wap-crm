18e23eeccd7fd37c60faffb8548eed9392691724
1176944648 <56572999+1176944648@users.noreply.github.com>
2020-01-16 17:56:13 +0800

    优化竞拍页面层级

--- src/pages/bidding/orderList/list.vue
+++ src/pages/bidding/orderList/list.vue
@@ -1,16 +1,13 @@
 <template>
   <div>
-    <Header title="订单竞价系统"/>
     <div>空状态/列表</div>
   </div>
 </template>
 
 <script>
-import Header from "../../../components/header/Header.vue";
+
 export default {
-  components: {
-    Header
-  }
+
 };
 </script>
 
--- src/router/routes.js
+++ src/router/routes.js
@@ -41,26 +41,30 @@ const router = [{
     path: "/search",
     name: "search",
     component: () => import("../pages/customerList/search/search.vue"),
-},
-{
-    path: "/orderlist",//竞拍订单列表
-    component: () => import("../pages/bidding/orderList/list.vue")
-},
-{
-    path: "/bidding",//订单竞拍/竞速
-    component: () => import("../pages/bidding/auction/biddingStage.vue"),
-},
-{
-    path: "/bidding/succeed",//竞拍成功
-    component: () => import("../pages/bidding/auction/succeed/succeed.vue"),
-},
-{
-    path: "/bidding/defeated",//竞拍失败
-    component: () => import("../pages/bidding/auction/defeated/defeated.vue"),
-},
-{
-    path: "/bidding/abortive",//流拍
-    component: () => import("../pages/bidding/auction/abortive/abortive.vue"),
+},{
+    path : "/biddingindex",//竞拍首页
+    component : ()=>import("../pages/bidding/index.vue"),
+    children : [{
+        path: "orderlist",//竞拍订单列表
+        component: () => import("../pages/bidding/orderList/list.vue")
+    },
+    {
+        path: "bidding",//订单竞拍/竞速
+        component: () => import("../pages/bidding/auction/biddingStage.vue"),
+    },
+    {
+        path: "bidding/succeed",//竞拍成功
+        component: () => import("../pages/bidding/auction/succeed/succeed.vue"),
+    },
+    {
+        path: "bidding/defeated",//竞拍失败
+        component: () => import("../pages/bidding/auction/defeated/defeated.vue"),
+    },
+    {
+        path: "bidding/abortive",//流拍
+        component: () => import("../pages/bidding/auction/abortive/abortive.vue"),
+    }],
+    redirect: "/biddingindex/orderlist",
 },
 {
     path: "/Details",
