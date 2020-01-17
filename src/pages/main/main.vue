2e2c058596d91484d9704a51d43aeb25756c18f0
dabai <571388225@qq.com>
2020-01-17 15:13:58 +0800

    更新首页
    完善首页

--- src/pages/main/main.vue
+++ src/pages/main/main.vue
@@ -1,100 +1,120 @@
 <template>
-
   <div id="main">
-    <Header :title="title"/>
+    <Header :title="title" />
+    <div class="city">
+      <van-dropdown-menu>
+        <van-dropdown-item v-model="value1" :options="option1" />
+      </van-dropdown-menu>
+    </div>
     <div class="picture"></div>
-    <div class="welcome">欢迎回来 </div>
+    <div class="welcome">欢迎回来</div>
     <ul>
-      <li class="list"> 
+      <li class="list">
         <router-link to="/customerList"></router-link>
         <!-- 跳转客户列表 -->
       </li>
       <li class="message">
         <router-link to="/information"></router-link>
-<!-- 信息录入暂时没有写 -->
+        <!-- 信息录入暂时没有写 -->
       </li>
       <li class="map">
         <router-link to="/clock"></router-link>
         <!-- 打卡（地图找房） -->
       </li>
       <li class="deal">
-        <router-link to="orderlist/:id"></router-link>
+        <router-link to="/bidding"></router-link>
         <!-- 订单竞拍 -->
       </li>
     </ul>
-     
   </div>
 </template>
 
 
 <script>
-import Header from "../../components/header/Header.vue"
+import Header from "../../components/header/Header.vue";
+// import { TreeSelect } from 'vant';
 
 export default {
-    components:{
-      Header
-    },
-    data(){
-      return{
-        title:"首页"
-      }
-    }
-}
+  components: {
+    Header
+    // [TreeSelect.name] : TreeSelect
+  },
+  data() {
+    return {
+      title: "首页",
+      value1: 0,
+      option1: [
+        { text: '上海', value: 0 },
+        { text: '北京', value: 1 },
+        { text: '南京', value: 2 },
+        { text: '杭州', value: 3 },
+        { text: '郑州', value: 4 },
+      ],
+    };
+  }
+};
 </script>
 
 
 <style lang="stylus" scoped>
-   #main
-     width 100%
-     height 100%
-    .picture
-       width 100%
-       height 2.7rem
-       border .02rem solid pink
-       background url(../../assets/img/bannar.jpg)  
-       background-size 100%
-    .welcome
-       width 7.5rem
-       height 1rem
-       background url(../../assets/img/welcome.png) no-repeat  
-       text-indent 0.6rem
-       line-height 1rem
-       border-bottom .02rem solid #ebebeb
-     ul
-       width 7.5rem
-       height 3.5rem
-       margin-top 0.32rem
-       li
-        float left
-        margin: 0 0.5rem 0.4rem;
-       .list
-        width 1.28rem
-        height  1.5rem
-        background url(../../assets/img/list.jpg)  
-        background-size: 100% 
-       .message
-        width 1.69rem
-        height  1.47rem
-        background url(../../assets/img/message.jpg)  
-        background-size: 100% 
-       .map
-        width 1.1rem
-        height  1.5rem
-        background url(../../assets/img/map.jpg)  
-        background-size: 100% 
-       .deal
-        width 1.69rem
-        height  1.5rem
-        background url(../../assets/img/deal.jpg)  
-        background-size: 100% 
-        margin-left 0.3rem
-
-      a
-      width 1.69rem
-      display block
-      height  1.47rem
-       
-        
-    
-     
+#main
+  width: 100%
+  height: 100%
+  position: relative
+.city
+  color #fff
+  position: absolute
+  right: 0.3rem
+  top: 0
+  .van-dropdown-menu
+   height 0.9rem
+   background none 
+  .van-ellipsis
+    color #fff
+ 
+.picture
+  width: 100%
+  height: 2.7rem
+  border: 0.02rem solid pink
+  background: url('../../assets/img/bannar.jpg')
+  background-size: 100%
+.welcome
+  width: 7.5rem
+  height: 1rem
+  background: url('../../assets/img/welcome.png') no-repeat
+  text-indent: 0.6rem
+  line-height: 1rem
+  border-bottom: 0.02rem solid #ebebeb
+ul
+  width: 7.5rem
+  height: 3.5rem
+  margin-top: 0.32rem
+  li
+    float: left
+    margin: 0 0.5rem 0.4rem
+  .list
+    width: 1.28rem
+    height: 1.5rem
+    background: url('../../assets/img/list.jpg')
+    background-size: 100%
+  .message
+    width: 1.69rem
+    height: 1.47rem
+    background: url('../../assets/img/message.jpg')
+    background-size: 100%
+  .map
+    width: 1.1rem
+    height: 1.5rem
+    background: url('../../assets/img/map.jpg')
+    background-size: 100%
+  .deal
+    width: 1.69rem
+    height: 1.5rem
+    background: url('../../assets/img/deal.jpg')
+    background-size: 100%
+    margin-left: 0.3rem
+a
+  width: 1.69rem
+  display: block
+  height: 1.47rem
 </style>
--- src/pages/register/loginn.vue
+++ src/pages/register/loginn.vue
@@ -1,22 +1,21 @@
 <template>
-
   <div id="big">
     <div class="bi">
-    <div class="title"></div>
-     <ul>
-       <li>
-         <input type="text"  placeholder="输入工号"  v-model="userid">
-       </li>
-      <li>
-         <input type="text"  placeholder="输入密码" v-model="password">
-       </li>
-       <li>
-         <input type="text"  placeholder="输入验证码" v-model="seccode">
-       </li>
-     </ul>
-     <div class="huo">获取验证码</div> 
-     
-    <router-link to="/index">登录</router-link>
+      <div class="title"></div>
+      <ul>
+        <li>
+          <input type="text" placeholder="输入工号" v-model="userid" />
+        </li>
+        <li>
+          <input type="text" placeholder="输入密码" v-model="password" />
+        </li>
+        <li>
+          <input type="text" placeholder="输入验证码" v-model="seccode" />
+        </li>
+      </ul>
+      <div class="huo">获取验证码</div>
+
+      <router-link to="/index">登录</router-link>
     </div>
   </div>
 </template>
@@ -24,63 +23,67 @@
 
 <script>
 export default {
-
-  data(){
-    return{
-      userid:'',
-      password:'',
-      seccode:''
-    }
+  data() {
+    return {
+      userid: "",
+      password: "",
+      seccode: ""
+    };
   }
 };
 </script>
 
 
 <style lang="stylus" scoped>
-#big 
-  height 100%
-  width 100%
-  background: url(../../assets/img/bei.jpg)  no-repeat
+#big
+  height: 100%
+  width: 100%
+  background: url('../../assets/img/bei.jpg') no-repeat
   background-size: 100% 100%
   .bi
     width: 6.5rem
     text-align: center
     margin: 0 auto
     padding-top: 4.2rem
-    color #fff
+    color: #fff
     .title
-      width 3.59rem
+      width: 3.59rem
       height: 2.07rem
-      background url(../../assets/img/title.jpg)  
-      background-size 100%
+      background: url('../../assets/img/title.jpg')
+      background-size: 100%
       margin: -2.3rem 0 1.5rem 1.5rem
     ul
-      width 6.5rem
-      height 3rem
-      border-radius .1rem
-      background-image url(../../assets/img/gonghao.png)  
-      background-size 100%
+      width: 6.5rem
+      height: 3rem
+      border-radius: 0.1rem
+      background-image: url('../../assets/img/gonghao.png')
+      background-size: 100%
+      border-radius: 2%;
+
       li
-        height 1rem
-        border-bottom  .02rem solid #e7e7e7 
-        input 
-          height 0.9rem
-          border none
-          color #000
+        height: 1rem
+        border-bottom: 0.02rem solid #e7e7e7
+        input
+          height: 0.9rem
+          border: none
+          color: #000
     .huo
-       width 6.5rem
-       height 0.94rem
-       line-height 0.94rem
-       margin-top 0.5rem
-       background #108ee9
-       color #fff
-      a
-       display block
-       width 6.5rem
-       height 0.94rem
-       line-height 0.94rem
-       margin-top 0.5rem
-       background #108ee9
-       color #fff
-       
+      width: 6.5rem
+      height: 0.94rem
+      line-height: 0.94rem
+      margin-top: 0.5rem
+      background: #108ee9
+      color: #fff
+      border-radius: 2%;
+
+    a
+      display: block
+      width: 6.5rem
+      height: 0.94rem
+      line-height: 0.94rem
+      margin-top: 0.5rem
+      background: #108ee9
+      color: #fff
+      border-radius: 2%;
+
 </style>
