ec9dda863eb3e5bec75e711c8bd5f25658bc842a
zhuhailiang <524577590@qq.com>
2020-01-17 11:32:10 +0800

    删除用户列表tab切换原样式,
    采用 v-for

--- src/pages/customerList/index.vue
+++ src/pages/customerList/index.vue
@@ -1,32 +1,28 @@
 <template>
   <div>
-    <Header :title="title" />
-    <router-link to="/search">搜索</router-link>
-    <div>
-      <nav class="tab">
-        <li v-for="data of titlList" 
-        :class="{active:data.active}" 
-        @click="changeActive(data.route)" 
-        :key="data.route">
-          <router-link :to="'/customerList/'+data.route"> {{data.name}}</router-link>
-        </li>
-        <!-- <li class="active">
-          <router-link to="/customerList/all">全部</router-link>
-        </li>
-        <li>
-          <router-link to="/customerList/new">新分派</router-link>
-        </li>
-        <li>
-          <router-link to="/customerList/follow">跟进中</router-link>
-        </li>
-        <li>
-          <router-link to="/customerList/sign">签约中</router-link>
-        </li>
-        <li>
-          <router-link to="/customerList/more">更多</router-link>
-        </li> -->
-      </nav>
+    <Header title="客户列表">
+      <div>
+        <router-link to="/search">
+          <img src="../../assets/img/search.png" alt />
+        </router-link>
+        <router-link to="/search">标签></router-link>
+      </div>
+    </Header>
 
+    <div>
+      <!-- tab切换 -->
+      <div class="tabWrap">
+        <nav class="tab">
+          <li
+            v-for="data of titlList"
+            :class="{active:data.active}"
+            @click="changeActive(data.route)"
+            :key="data.route"
+          >
+            <router-link :to="'/customerList/'+data.route">{{data.name}}</router-link>
+          </li>
+        </nav>
+      </div>
       <router-view />
     </div>
   </div>
@@ -36,27 +32,29 @@
 import Header from "../../components/header/Header.vue";
 export default {
   components: {
-    Header,
+    Header
   },
   data() {
     return {
-      title: "客户列表",
-      titlList:[{route:"all",name:"全部",active:true},
-            {route:"new",name:"新分派",active:false},
-            {route:"follow",name:"跟进中",active:false},
-            {route:"sign",name:"签约中",active:false},
-            {route:"more",name:"更多",active:false}],
-      };
+      titlList: [
+        { route: "all", name: "全部", active: true },
+        { route: "new", name: "新分派", active: false },
+        { route: "follow", name: "跟进中", active: false },
+        { route: "sign", name: "签约中", active: false },
+        { route: "more", name: "更多", active: false }
+      ]
+    };
   },
-  methods:{
-    changeActive(route){
+  methods: {
+    changeActive(route) {
       // console.log(this.titlList);
-      
-      for(let data of this.titlList){//切换tab active 状态
-        if(data.route==route){
-          data.active = true
-        }else{
-          data.active = false
+
+      for (let data of this.titlList) {
+        //切换tab active 状态
+        if (data.route == route) {
+          data.active = true;
+        } else {
+          data.active = false;
         }
       }
     }
@@ -65,32 +63,36 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-.tab {
-  width: 100%;
-  height: 0.84rem;
-  list-style: none;
-  display: flex;
-
-  li {
-    display: flex;
-    width: 1.5rem;
-    height: 100%;
-    justify-content: center;
-
-    a {
-      display: flex;
-      align-self: center;
-      font-size: 0.28rem;
-      color: #333;
-    }
-  }
-
-  .active {
-    border-bottom: 0.04rem solid #108ee9;
-
-    a {
-      color: #108ee9;
-    }
-  }
-}
+Header a img
+  height: 0.31rem
+  width: 0.31rem
+  margin-right: 0.4rem
+  position: relative
+  top: 0.07rem
+.tabWrap
+  position: relative
+  height: 0.84rem
+.tab
+  width: 100%
+  height: 0.84rem
+  list-style: none
+  display: flex
+  position: absolute
+  top: 0
+  left: 0
+  z-index: 10
+  li
+    display: flex
+    width: 1.5rem
+    height: 100%
+    justify-content: center
+    a
+      display: flex
+      align-self: center
+      font-size: 0.28rem
+      color: #333
+  .active
+    border-bottom: 0.04rem solid #108ee9
+    a
+      color: #108ee9
 </style>
